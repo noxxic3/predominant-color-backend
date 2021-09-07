5c7d14973fcacbb8871d49103bff981279b5f937
“noxxic3” <noxxic3@gmail.com>
2021-09-08 00:29:46 +0200

    Comparison of the predominant color with the closest palette color done

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -76,12 +76,63 @@ class ColorController extends Controller
         */
         $topRepresentativeColor = Color::fromIntToHex( $topRepresentativeColors[0] );
         //return gettype( $topRepresentativeColor );       // String
-        return $topRepresentativeColor;                   // <<<------
+        //return $topRepresentativeColor;                   // <<<------
 
         //return $request->file('image')->getClientOriginalName();
         //return $request->file('image');
         //return $request->input('image');
         //return $request;
+
+        /* Compare the color with the colors provided */
+
+        // https://www.w3schools.com/php/php_arrays_associative.asp
+
+        $colorPalette = array(
+            "Aqua"=>"#00FFFF",
+            "Black"=>"#000000",
+            "Blue"=>"#0000FF",
+            "Fuchsia"=>"#FF00FF",
+            "Navy"=>"#000080",
+            "Olive"=>"#808000",
+            "Purple"=>"#800080",
+            "Red"=>"#FF0000",
+            "Gray"=>"#808080",
+            "Green"=>"#008000",
+            "Lime"=>"#00FF00",
+            "Maroon"=>"#800000",
+            "Silver"=>"#C0C0C0",
+            "Teal"=>"#008080",
+            "White"=>"#FFFFFF",
+            "Yellow"=>"#FFFF00"
+        );
+
+        // Convert hexadecimal to RGB numbering system
+        $r = hexdec( substr($topRepresentativeColor,1,2) );
+        $g = hexdec( substr($topRepresentativeColor,3,2) );
+        $b = hexdec( substr($topRepresentativeColor,5,2) );
+
+        //return $topRepresentativeColor . ' ' . $r . ' ' . $g . ' ' . $b;
+
+        $colorPaletteDistances = [];
+        foreach($colorPalette as $key => $value) {
+
+            $palette_r = hexdec( substr($value,1,2) );
+            $palette_g = hexdec( substr($value,3,2) );
+            $palette_b = hexdec( substr($value,5,2) );
+
+            // Get the diference of the RGB components of the compared colors
+            $distance = abs($r - $palette_r) + abs($g - $palette_g) + abs($b - $palette_b);
+
+            // Mount new array with the distance of every palette color
+            $colorPaletteDistances += [$key => $distance];
+        }
+
+        //return $colorPaletteDistances;
+        //return min($colorPaletteDistances);
+        // Takes the minimum value of the color palette distances and returns the key (color name) of the color that has this smallest distance to the compared color
+        $closestColor = array_keys($colorPaletteDistances, min($colorPaletteDistances));
+
+        return [ $closestColor[0] => $colorPalette[$closestColor[0]] ];
 
     }
 
